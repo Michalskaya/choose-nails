8257744d91852ee9e61116abebc2f6f7171cf184
Michalskaya <michaltse132@gmail.com>
2024-07-25 15:18:19 +0300

    Add files via upload

---
+++ nails.py
@@ -0,0 +1,27 @@
+import random
+import webbrowser
+import urllib.parse
+
+# Lists of colors and designs
+colors = ["Red", "Pink", "Orange", "Yellow", "Green", "Blue", "Purple", "Black", "White", "Silver", "Gold"]
+designs = ["Floral", "French tips", "Wiggly lines", "Glitter", "3D texture", "Hearts", "Jewels", "Animal print"]
+
+def generate_nail_idea():
+    color = random.choice(colors)
+    design = random.choice(designs)
+    return color, design
+
+def search_pinterest(color, design):
+    query = f"{color} {design} nails"
+    encoded_query = urllib.parse.quote(query)
+    url = f"https://www.pinterest.com/search/pins/?q={encoded_query}"
+    webbrowser.open(url)
+
+# Generate a nail idea
+color, design = generate_nail_idea()
+
+# Print the result
+print(f"Your random nail design idea:\nColor: {color}\nDesign: {design}")
+
+# Search on Pinterest
+search_pinterest(color, design)
