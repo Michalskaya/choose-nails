3f81d90a08de3f05bc5e61971255001b89263bd1
Michalskaya <michaltse132@gmail.com>
2024-07-25 15:39:24 +0300

    Add files via upload

--- nails.py
+++ nails.py
@@ -3,8 +3,9 @@ import webbrowser
 import urllib.parse
 
 # Lists of colors and designs
-colors = ["Red", "Pink", "Orange", "Yellow", "Green", "Blue", "Purple", "Black", "White", "Silver", "Gold"]
-designs = ["Floral", "French tips", "Wiggly lines", "Glitter", "3D texture", "Hearts", "Jewels", "Animal print"]
+colors = ["Red", "Pink", "Orange", "Yellow", "Green", "Blue", "Purple", "Black", "White", "Lavender", "Nude", "Neon", "Pastel"]
+designs = ["Floral", "French tips", "Wiggly lines", "Glitter", "3D texture", "Hearts", "Ombre", "Animal print", "Marble Effect", "Polka Dots", "Stripes", "Glitter Gradient",
+"Watercolor", "Matte Finish", "Fruit Design"]
 
 def generate_nail_idea():
     color = random.choice(colors)
